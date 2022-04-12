fdbbd4b6ebfe33ebee7da9fcec66c3d485131281
Rubenennj <57841770+Rubenennj@users.noreply.github.com>
2022-04-12 13:38:33 +0200

    Applied indentation

--- src/commands/staff/color.ts
+++ src/commands/staff/color.ts
@@ -1,43 +1,58 @@
 import { MessageEmbed, MessageAttachment } from "discord.js";
 import { Command } from "../../structures/Command";
-import { createCanvas, loadImage } from 'canvas';
+import { createCanvas } from 'canvas';
 import StaffRoles from "../../util/constants/StaffRoles";
 
-export default new Command<[string]>({
+export default new Command<[
+    string
+]>({
     name: 'color',
     aliases: [
         "colour",
         "col"
     ],
     description: 'get color from a given value. accepts RGB(A) and Hex values.',
-    args: [{
+    args: [
+        {
             name:'value',
             required: true,
             type:'STRING'
-           }],
+        }
+    ],
     roles: [ 
-          StaffRoles.ADMIN_TEST, 
-          StaffRoles.STAFF 
-  ],
+        StaffRoles.ADMIN_TEST, 
+        StaffRoles.STAFF 
+    ],
     execute: async function(message, [ value ]){
         const canvas = createCanvas(200, 200)
         const ctx = canvas.getContext('2d')
+
         ctx.fillStyle = value
+        
         ctx.rect(0, 0, 200, 200)
         ctx.fill()
         
         const att = new MessageAttachment(canvas.toBuffer(), 'color.png');
         const embed = new MessageEmbed()
-        .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+        .setAuthor({
+            name: message.author.tag, 
+            iconURL: message.author.displayAvatarURL({ dynamic: true })
+        })
         .setDescription(`**Color Given**: ${value}`)
         .setImage('attachment://color.png')
         .setColor('#2f2136')
         .setTimestamp()
 
         await message.reply({
-            files:[att], 
-            embeds:[embed], 
-            allowedMentions: { repliedUser: false }
+            files: [
+                att
+            ], 
+            embeds: [
+                embed
+            ], 
+            allowedMentions: {
+                repliedUser: false
+            }
         })
      }
 })
